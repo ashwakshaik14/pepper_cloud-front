045e046ec05e82a3b307a633106070bd6d21b9b4
ashwakshaik14 <ashwakshaik15@gmail.com>
2025-03-23 03:30:28 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'node:process';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173,
+    host: "0.0.0.0",
+    allowedHosts: ["https://pepper-cloud-front.onrender.com"], // Allow your Render domain
+  },
+  preview: {
+    port: process.env.PORT || 4173,
+    host: "0.0.0.0",
+  },
+});
